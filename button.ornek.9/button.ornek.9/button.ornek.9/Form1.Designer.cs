04f2699de43a9eeaafdef4589ef8adb725bf42d9
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-09-26 14:43:23 +0300

    Add files via upload

---
+++ button.ornek.9/button.ornek.9/button.ornek.9/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace button.ornek._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.btnToplam = new System.Windows.Forms.Button();
+            this.btnCikarma = new System.Windows.Forms.Button();
+            this.btnCarpma = new System.Windows.Forms.Button();
+            this.btnBolme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(133, 48);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(133, 123);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(41, 204);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(85, 25);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "Sonuç=";
+            this.lblSonuc.Click += new System.EventHandler(this.lblSonuc_Click);
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.AutoSize = true;
+            this.lblSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi1.Location = new System.Drawing.Point(29, 48);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(60, 24);
+            this.lblSayi1.TabIndex = 5;
+            this.lblSayi1.Text = "Sayi1 ";
+            this.lblSayi1.Click += new System.EventHandler(this.lblSayi1_Click);
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.AutoSize = true;
+            this.lblSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi2.Location = new System.Drawing.Point(29, 123);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(55, 24);
+            this.lblSayi2.TabIndex = 6;
+            this.lblSayi2.Text = "Sayi2";
+            // 
+            // btnToplam
+            // 
+            this.btnToplam.Location = new System.Drawing.Point(310, 48);
+            this.btnToplam.Name = "btnToplam";
+            this.btnToplam.Size = new System.Drawing.Size(103, 76);
+            this.btnToplam.TabIndex = 7;
+            this.btnToplam.Text = "Toplam";
+            this.btnToplam.UseVisualStyleBackColor = true;
+            this.btnToplam.Click += new System.EventHandler(this.btnToplam_Click);
+            // 
+            // btnCikarma
+            // 
+            this.btnCikarma.Location = new System.Drawing.Point(447, 48);
+            this.btnCikarma.Name = "btnCikarma";
+            this.btnCikarma.Size = new System.Drawing.Size(103, 76);
+            this.btnCikarma.TabIndex = 8;
+            this.btnCikarma.Text = "Çıkarma";
+            this.btnCikarma.UseVisualStyleBackColor = true;
+            this.btnCikarma.Click += new System.EventHandler(this.btnCikarma_Click);
+            // 
+            // btnCarpma
+            // 
+            this.btnCarpma.Location = new System.Drawing.Point(310, 153);
+            this.btnCarpma.Name = "btnCarpma";
+            this.btnCarpma.Size = new System.Drawing.Size(103, 76);
+            this.btnCarpma.TabIndex = 9;
+            this.btnCarpma.Text = "Çarpma";
+            this.btnCarpma.UseVisualStyleBackColor = true;
+            this.btnCarpma.Click += new System.EventHandler(this.btnCarpma_Click);
+            // 
+            // btnBolme
+            // 
+            this.btnBolme.Location = new System.Drawing.Point(447, 153);
+            this.btnBolme.Name = "btnBolme";
+            this.btnBolme.Size = new System.Drawing.Size(103, 76);
+            this.btnBolme.TabIndex = 10;
+            this.btnBolme.Text = "Bölme";
+            this.btnBolme.UseVisualStyleBackColor = true;
+            this.btnBolme.Click += new System.EventHandler(this.btnBolme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 380);
+            this.Controls.Add(this.btnBolme);
+            this.Controls.Add(this.btnCarpma);
+            this.Controls.Add(this.btnCikarma);
+            this.Controls.Add(this.btnToplam);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblSayi1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Name = "Form1";
+            this.Text = "Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Button btnToplam;
+        private System.Windows.Forms.Button btnCikarma;
+        private System.Windows.Forms.Button btnCarpma;
+        private System.Windows.Forms.Button btnBolme;
+    }
+}
+
---
+++ button.ornek.9/button.ornek.9/button.ornek.9/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnToplam_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 + sayi2;
+
+            lblSonuc.Text=sonuc.ToString();
+              
+
+        }
+
+        private void lblSayi1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCikarma_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 - sayi2;
+
+            lblSonuc.Text = sonuc.ToString();
+
+        }
+
+        private void lblSonuc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCarpma_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 * sayi2;
+
+            lblSonuc.Text = sonuc.ToString();
+        }
+
+        private void btnBolme_Click(object sender, EventArgs e)
+        {
+            float sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToSingle(txtSayi1.Text);
+            sayi2 = Convert.ToSingle(txtSayi2.Text);
+            sonuc = sayi1 / sayi2;
+
+            lblSonuc.Text = sonuc.ToString();
+        }
+    }
+}
